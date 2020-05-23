b38fbfb3f6ff379375c22bd150cf665aaeed9fde
Dídac García <didaquis@gmail.com>
2020-05-23 21:14:08 +0200

    Added more test

--- tests/package.test.js
+++ tests/package.test.js
@@ -9,20 +9,40 @@ describe('package.json file', () => {
 		packageJSONData = jsonfile.readFileSync(file);
 	});
 
-	test('Should have all devDependencies with semver version fixed', () => {
-		const validPattern = /^(\d+\.)(\d+\.)(\d+)$/;
-		const regex = RegExp(validPattern);
-
-		let allDevDependenciesAreFixed = true;
-
-		for (let key in packageJSONData.devDependencies){
-			if (Object.prototype.hasOwnProperty.call(packageJSONData.devDependencies, key)) {
-				if (!regex.test(packageJSONData.devDependencies[key])) {
-					allDevDependenciesAreFixed = false;
+	test('Should have all dependencies with semver version fixed', () => {
+		if (packageJSONData.dependencies) {
+			const validPattern = /^(\d+\.)(\d+\.)(\d+)$/;
+			const regex = RegExp(validPattern);
+	
+			let allDependenciesAreFixed = true;
+			for (let key in packageJSONData.dependencies){
+				if (Object.prototype.hasOwnProperty.call(packageJSONData.dependencies, key)) {
+					if (!regex.test(packageJSONData.dependencies[key])) {
+						allDependenciesAreFixed = false;
+					}
 				}
 			}
+	
+			expect(allDependenciesAreFixed).toBe(true);
 		}
+	});
 
-		expect(allDevDependenciesAreFixed).toBe(true);
+	test('Should have all devDependencies with semver version fixed', () => {
+		if (packageJSONData.devDependencies) {
+			const validPattern = /^(\d+\.)(\d+\.)(\d+)$/;
+			const regex = RegExp(validPattern);
+
+			let allDevDependenciesAreFixed = true;
+
+			for (let key in packageJSONData.devDependencies){
+				if (Object.prototype.hasOwnProperty.call(packageJSONData.devDependencies, key)) {
+					if (!regex.test(packageJSONData.devDependencies[key])) {
+						allDevDependenciesAreFixed = false;
+					}
+				}
+			}
+
+			expect(allDevDependenciesAreFixed).toBe(true);
+		}
 	});
 });
